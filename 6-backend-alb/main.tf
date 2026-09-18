0e042c37f160e3d80d1ed03c940c3360dcf9ec18
nikitha84 <nikitha84640@gmail.com>
2026-09-18 12:31:28 +0530

    catalogue

--- 6-backend-alb/main.tf
+++ 6-backend-alb/main.tf
@@ -1,5 +1,5 @@
 resource "aws_lb" "backend_alb" {
-  name               = "${local.common_name_suffix}-backend_alb" #roboshop-dev-backend-alb
+  name               = "${local.common_name_suffix}-backend-alb" #roboshop-dev-backend-alb
   internal           = true
   load_balancer_type = "application"
   security_groups    = [local.backend_alb_sg_id]
@@ -10,7 +10,7 @@ resource "aws_lb" "backend_alb" {
     tags = merge(
         local.common_tags,
         {
-            Name = "${local.common_name_suffix}-backend_alb"
+            Name = "${local.common_name_suffix}-backend-alb"
         }
 
     )
