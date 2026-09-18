e413146b31f1a4aad02872487ca2fa34fdc09564
nikitha84 <nikitha84640@gmail.com>
2026-09-18 11:11:15 +0530

    catalogue_sg

--- 4-sg-rules/main.tf
+++ 4-sg-rules/main.tf
@@ -54,3 +54,31 @@ resource "aws_security_group_rule" "mysql_bastion" {
   security_group_id = local.mysql_sg_id
   
 }
+resource "aws_security_group_rule" "catalogue_bastion" {
+  type              = "ingress"
+  from_port         =  22
+  to_port           = 22
+  protocol          = "tcp"
+  source_security_group_id =  local.bastion_sg_id
+  security_group_id = local.catalogue_sg_id
+  
+}
+resource "aws_security_group_rule" "mongodb_catalogue" {
+  type              = "ingress"
+  from_port         =  27017
+  to_port           = 27017
+  protocol          = "tcp"
+  source_security_group_id =  local.catalogue_sg_id
+  security_group_id = local.mongodb_sg_id
+  
+}
+
+resource "aws_security_group_rule" "catalogue_backend_alb" {
+  type              = "ingress"
+  from_port         =  8080
+  to_port           = 8080
+  protocol          = "tcp"
+  source_security_group_id =  local.backend_alb_sg_id
+  security_group_id = local.catalogue_sg_id
+  
+}
